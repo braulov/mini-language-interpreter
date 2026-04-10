f10b5651de3fbb2666d7f432dc0db421b07de0c3
Grigorii Braulov <mr.braulov@mail.ru>
2026-04-10 10:34:04 +0300

    Implement scanner-based lexer

--- src/main/kotlin/lexer/Lexer.kt
+++ src/main/kotlin/lexer/Lexer.kt
@@ -9,27 +9,124 @@ class Lexer(
     private var current = 0
     private var line = 1
 
-    fun scanTokens(): List<Token> = TODO("Not yet implemented")
+    fun scanTokens(): List<Token> {
+        while (!isAtEnd()) {
+            start = current
+            scanToken()
+        }
 
-    private fun scanToken() : Nothing = TODO("Not yet implemented")
+        tokens += Token(
+            type = TokenType.EOF,
+            lexeme = "",
+            literal = null,
+            line = line,
+        )
 
-    private fun addToken(type: TokenType, literal: Any? = null): Nothing =
-        TODO("Not yet implemented")
+        return tokens
+    }
 
-    private fun advance(): Char = TODO("Not yet implemented")
+    private fun scanToken() {
+        when (val c = advance()) {
+            '(' -> addToken(TokenType.LEFT_PAREN)
+            ')' -> addToken(TokenType.RIGHT_PAREN)
+            '{' -> addToken(TokenType.LEFT_BRACE)
+            '}' -> addToken(TokenType.RIGHT_BRACE)
+            ',' -> addToken(TokenType.COMMA)
 
-    private fun match(expected: Char): Boolean =
-        TODO("Not yet implemented")
+            '+' -> addToken(TokenType.PLUS)
+            '-' -> addToken(TokenType.MINUS)
+            '*' -> addToken(TokenType.STAR)
+            '/' -> addToken(TokenType.SLASH)
 
-    private fun peek(): Char? = TODO("Not yet implemented")
+            '=' -> addToken(if (match('=')) TokenType.EQUAL_EQUAL else TokenType.ASSIGN)
+            '!' -> {
+                if (match('=')) {
+                    addToken(TokenType.NOT_EQUAL)
+                } else {
+                    throw IllegalArgumentException("Unexpected character '!' at line $line")
+                }
+            }
+            '<' -> addToken(if (match('=')) TokenType.LESS_EQUAL else TokenType.LESS)
+            '>' -> addToken(if (match('=')) TokenType.GREATER_EQUAL else TokenType.GREATER)
 
-    private fun peekNext(): Char? = TODO("Not yet implemented")
+            ' ', '\r', '\t' -> Unit
 
-    private fun number(): Nothing = TODO("Not yet implemented")
+            '\n' -> {
+                addToken(TokenType.NEWLINE)
+                line++
+            }
 
-    private fun identifier(): Nothing = TODO("Not yet implemented")
+            else -> when {
+                c.isDigit() -> number()
+                isIdentifierStart(c) -> identifier()
+                else -> throw IllegalArgumentException("Unexpected character '$c' at line $line")
+            }
+        }
+    }
 
-    private fun isAtEnd(): Boolean = TODO("Not yet implemented")
+    private fun addToken(type: TokenType, literal: Any? = null) {
+        val lexeme = source.substring(start, current)
+        tokens += Token(
+            type = type,
+            lexeme = lexeme,
+            literal = literal,
+            line = line,
+        )
+    }
+
+    private fun advance(): Char {
+        return source[current++]
+    }
+
+    private fun match(expected: Char): Boolean {
+        if (isAtEnd()) return false
+        if (source[current] != expected) return false
+
+        current++
+        return true
+    }
+
+    private fun peek(): Char? {
+        return if (isAtEnd()) null else source[current]
+    }
+
+    private fun peekNext(): Char? {
+        return if (current + 1 >= source.length) null else source[current + 1]
+    }
+
+    private fun number() {
+        while (peek()?.isDigit() == true) {
+            advance()
+        }
+
+        val lexeme = source.substring(start, current)
+        addToken(
+            type = TokenType.INT,
+            literal = lexeme.toInt(),
+        )
+    }
+
+    private fun identifier() {
+        while (peek()?.let(::isIdentifierPart) == true) {
+            advance()
+        }
+
+        val lexeme = source.substring(start, current)
+        val type = keywords[lexeme] ?: TokenType.IDENTIFIER
+        addToken(type)
+    }
+
+    private fun isAtEnd(): Boolean {
+        return current >= source.length
+    }
+
+    private fun isIdentifierStart(c: Char): Boolean {
+        return c.isLetter() || c == '_'
+    }
+
+    private fun isIdentifierPart(c: Char): Boolean {
+        return c.isLetterOrDigit() || c == '_'
+    }
 
     private companion object {
         val keywords = mapOf(
